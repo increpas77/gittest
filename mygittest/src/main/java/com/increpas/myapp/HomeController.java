ff8a2dd8eb986314d46a6b083f571e11cc6ed21b
T07-N <T07-N@T07-N-PC>
2018-11-30 13:12:20 +0900

    ff

--- mygittest/src/main/java/com/increpas/myapp/HomeController.java
+++ mygittest/src/main/java/com/increpas/myapp/HomeController.java
@@ -24,7 +24,7 @@ public class HomeController {
 	 */
 	@RequestMapping(value = "/", method = RequestMethod.GET)
 	public String home(Locale locale, Model model) {
-		logger.info("홈! The client locale is {}.", locale);
+		logger.info("aa! The client locale is {}.", locale);
 		
 		Date date = new Date();
 		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
